7e64ee32259dce79587e2743c5efd6887aa353eb
naikgautamg@gmail.com <naikgautamg@gmail.com>
2018-12-06 18:52:28 -0500

    Start and Verify Pages

--- mobileApp/src/app/app.module.ts
+++ mobileApp/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { StartPage } from '../pages/start/start';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    StartPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    StartPage
   ],
   providers: [
     StatusBar,
